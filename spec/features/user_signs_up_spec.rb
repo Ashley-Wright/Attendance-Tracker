4a53564135a9f5ba25f70a1bb74ec1d6b04c9b3b
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-06-07 14:51:37 -0500

    User sign up feature

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature "User signs up" do
+  scenario "successful sign up" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "Email", with: "joe@example.com"
+    fill_in "user_password", with: "password"
+    fill_in "user_password_confirmation", with: "password"
+    click_button "Sign up"
+    page.should have_content "Welcome! You have signed up successfully."
+  end
+end
