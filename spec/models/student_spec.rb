70ea74f54df2af928c63598978b882c89e5e1724
Ashley Wright <AshleyMarieEvans@gmail.com>
2014-09-18 09:47:34 -0500

    Remove require spec_helper from model specs

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe Student do
   it { should have_many :attendances }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 describe User do
   it { should validate_uniqueness_of :email }
 end
